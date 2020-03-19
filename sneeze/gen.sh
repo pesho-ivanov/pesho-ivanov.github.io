e9b55c1790245529f271b23d916dbf9cedad306e
Pesho Ivanov <pesho@inf.ethz.ch>
2020-03-19 18:14:41 +0100

    ru

--- sneeze/gen.sh
+++ sneeze/gen.sh
@@ -8,3 +8,6 @@ convert $1_color.svg $1_color.png
 # Black and white
 sed "s/#bd4628/#000000/g" <<< `cat $1_color.svg` >tmp_bw.svg
 cairosvg tmp_bw.svg -o $1_bw.pdf
+
+
+# convert -verbose -density 150 -trim in.pdf -quality 100 -flatten -sharpen 0x1.0 out.png
