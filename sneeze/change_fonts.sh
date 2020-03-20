ca454b4006fa6b917a356032cf9d6b22a3da3c74
Pesho Ivanov <pesho@inf.ethz.ch>
2020-03-20 01:41:48 +0100

    fonts fixed

--- sneeze/change_fonts.sh
+++ sneeze/change_fonts.sh
@@ -1,3 +1,6 @@
+#for f in *.svg; do ./change_fonts.sh $f; done
+
+echo "Changing fonts of $1"
 sed "s/Noto Serif/Times/g" <<< `cat $1` >tmp.svg
 sed "s/Scada/Arial/g" <<< `cat tmp.svg` >tmp2.svg
 #rm tmp.svg
